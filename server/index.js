8144246d9138f29fe15d6c062aa0853ee8b60b49
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-11-11 16:41:28 -0800

    FEAT: added all the create pages

--- server/index.js
+++ server/index.js
@@ -10,20 +10,65 @@ const db = mysql.createConnection({
     user: "react",
     host: "localhost",
     password: "reactpassword",
-    database: "roommatesystem",
+    database: "database",
 });
 
-
-app.post('/create' , (req, res) => {
+//Adds account to database
+app.post('/createAccount' , (req, res) => {
     const name = req.body.name;
-    const age = req.body.age;
+    const emailAddress = req.body.emailAddress;
+    const telephone = req.body.telephone;
 
 
-    db.query('INSERT INTO roommates (name,age) VALUES (?,?)', [name,age], 
+    db.query('INSERT INTO account (name,emailAddress,telephone) VALUES (?,?,?)', [name,emailAddress,telephone], 
         (err,result) => {
             if (err){
                 console.log(err);
             } else{
+                res.send("Values Inserted");
+            }
+        })
+})
+
+//Adds house to database
+app.post('/createHouse' , (req, res) => {
+    const name = req.body.name;
+    const numberOfTenants = req.body.numberOfTenants;
+
+
+    db.query('INSERT INTO house (name,numberOfTenants) VALUES (?,?)', [name,numberOfTenants], 
+        (err,result) => {
+            if (err){
+                console.log(err);
+            } else{
+                res.send("Values Inserted");
+            }
+        })
+})
+
+//Adds house to database
+app.post('/createChore' , (req, res) => {
+    const title = req.body.title;
+    const assignedTo = req.body.assignedTo;
+    const dateAssigned = req.body.dateAssigned;
+    const completionStatus = req.body.completionStatus;
+    const priority = req.body.priority;
+    const choreStatus = req.body.choreStatus;
+
+
+    db.query('INSERT INTO choresmanagement (title,assignedTo,dateAssigned,completionStatus) VALUES (?,?,?,?)', [title,assignedTo,dateAssigned,completionStatus], 
+        (err,result) => {
+            if (err){
+                console.log(err);
+            } else{
+                db.query('INSERT INTO chore (priority,choreStatus) VALUES (?,?)',[priority,choreStatus],
+                (err,result) => {
+                    if (err){
+                        console.log(err);
+                    } else{
+                        res.send("Values Inserted");
+                    }
+                })
                 res.send("Values Inserted");
             }
         })
