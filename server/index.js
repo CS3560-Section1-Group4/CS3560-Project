14bdf154ed2982dd1c0aa69ea8dd4fcb3f56a8de
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-11-22 20:32:26 -0800

    FIX: added some crazy backend code that works

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.post('/createAccount' , (req, res) => {
     const emailAddress = req.body.emailAddress;
     const password = req.body.password;
 
-
     db.query('INSERT INTO account (name,emailAddress,password) VALUES (?,?,?)', [name,emailAddress,password], 
         (err,result) => {
             if (err){
@@ -28,22 +27,60 @@ app.post('/createAccount' , (req, res) => {
                 res.send("Values Inserted");
             }
         })
+    
+
 })
 
 //Adds house to database
 app.post('/createHouse' , (req, res) => {
     const name = req.body.name;
     const numberOfTenants = req.body.numberOfTenants;
+    var accountID = [];
+    var houseID = [];
 
-
-    db.query('INSERT INTO house (name,numberOfTenants) VALUES (?,?)', [name,numberOfTenants], 
+    db.query("SELECT accountID FROM account WHERE name=(?) ",[name], 
+    (err,result) => {
+        if (err) {
+            console.log(err);
+        } else{
+            setValue(result);
+        }
+    });
+    function setValue(value) {
+        accountID = value[0].accountID;
+        console.log(accountID);
+        db.query('INSERT INTO house (accountID,name,numberOfTenants) VALUES (?,?,?)', [accountID,name,numberOfTenants], 
         (err,result) => {
             if (err){
                 console.log(err);
             } else{
-                res.send("Values Inserted");
+                console.log("Holy shit we are in setValue")
             }
         })
+
+        db.query("SELECT houseID FROM house WHERE accountID=(?) ",[accountID], 
+        (err,result) => {
+            if (err) {
+                console.log(err);
+            } else{
+                setHouseValue(result);
+            }
+        });
+
+        function setHouseValue(value){
+            houseID = value[0].houseID;
+            console.log(houseID);
+            db.query('INSERT INTO housemates (accountID,houseID) VALUES (?,?)', [accountID,houseID], 
+            (err,result) => {
+                if (err){
+                    console.log(err);
+                } else{
+                    res.send("Values Inserted HouseValue");
+                }
+            })
+        }
+        
+    }
 })
 
 //Adds a new chore to database
@@ -51,9 +88,20 @@ app.post('/createChore' , (req, res) => {
     const title = req.body.title;
     const assignedTo = req.body.assignedTo;
     const completionStatus = req.body.completionStatus;
-
-
-    db.query('INSERT INTO chores (title,assignedTo,completionStatus) VALUES (?,?,?)', [title,assignedTo,completionStatus], 
+    var accountID = req.body.accountID;
+    var houseID = [];
+    db.query("SELECT houseID FROM house WHERE accountID=(?) ",[accountID], 
+    (err,result) => {
+        if (err) {
+            console.log(err);
+        } else{
+            setValue(result);
+        }
+    });
+    function setValue(value){
+        houseID = value[0].houseID;
+        console.log(houseID);
+        db.query('INSERT INTO chore (houseID,title,assignedTo,completionStatus,accountID) VALUES (?,?,?,?,?)', [houseID,title,assignedTo,completionStatus,accountID], 
         (err,result) => {
             if (err){
                 console.log(err);
@@ -61,25 +109,35 @@ app.post('/createChore' , (req, res) => {
                 res.send("Values Inserted");
             }
         })
+    }
+    
 })
 
 //checks if emailAddress and password of current user exists in the database
 app.post('/checkLogin', (req,res) => {
     const emailAddress = req.body.emailAddress;
     const password = req.body.password;
-    db.query("SELECT (?,?) FROM account",[emailAddress,password], 
+    var accountID = [];
+    db.query("SELECT accountID FROM account WHERE (emailAddress,password) = (?,?)",[emailAddress,password], 
     (err,result) => {
         if (err) {
             console.log(err);
+            res.send("Invalid login");
         } else{
-            res.send("Login is valid");
+            setValue(result);
+            res.send(accountID.toString());
+            
         }
     });
+    function setValue(value){
+        accountID = value[0].accountID;
+        console.log(accountID);
+    }
 });
 
 //returns all chores from the chores table in the database
 app.get('/getChores', (req,res) => {
-    db.query("SELECT * FROM chores", (err,result) => {
+    db.query("SELECT * FROM chore", (err,result) => {
         if (err) {
             console.log(err);
         } else{
@@ -101,7 +159,7 @@ app.get('/getHousemates', (req,res) => {
 
 //returns all houses from the houses table in the database
 app.get('/getHouses', (req,res) => {
-    db.query("SELECT * FROM houses", (err,result) => {
+    db.query("SELECT * FROM house", (err,result) => {
         if (err) {
             console.log(err);
         } else{
